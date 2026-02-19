4f761a411f925fcd2ad1356c174c9baa17f06381
Nithinkumarva <152359686+Nithinkumarva@users.noreply.github.com>
2026-02-19 14:44:34 +0530

    Add solution for Linear Search

---
+++ Easy/linear-search.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def linearSearch(arr: List[int], target: int) -> int:
+    # Your code here
+    pass
+
+# Read input
+arr = list(map(int, input().split()))
+target = int(input())
+print(linearSearch(arr, target))
